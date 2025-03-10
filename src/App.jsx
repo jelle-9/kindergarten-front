ee0fb220030ce62475ca8746b8a766a9981852e7
ousmane sow <thifuge@gmail.com>
2025-03-10 14:51:58 +0100

    Daten aus backend abholt

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import Navbarlayout from './layouts/Navbarlayout'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [gruppen, setGruppe] = useState([]);
 
+  useEffect(() => {
+    loadGruppen();
+  }, [])
 
+  const loadGruppen = async() =>{
+
+      const result = await axios.get("http://localhost:8080/getAllGruppe");
+      console.log(result.data)
+    setGruppe(result.data);
+  } 
   return (
     <>
       <Navbarlayout />
+
+      <ul>
+        {
+          
+        gruppen.map((gruppe) => (
+          <li>{gruppe.gruppeName}</li>
+        ))
+
+        }
+      </ul>
+    
     </>
   )
 }
