c87bb9de77ae5c9d928a7c9e1e9785fc630e14f6
ousmane sow <thifuge@gmail.com>
2025-03-11 14:39:36 +0100

    Accordion succes implementiert

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+
 export default function Navigator(props) {
-    
+
     const liste = props.liste;
    // const elemente = props.elemente;
   return (
@@ -14,69 +15,28 @@ export default function Navigator(props) {
             </ListGroup.Item>
         </ListGroup>
 
-        <Accordion.Item >
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-                <ul>
-                    <li>Jamal</li>
-                    <li>Evgeny</li>
-                    <li>Angelina</li>
-                    <li>Ousmane</li>
-                </ul>
-            </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="b">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-                <ListGroup.Item action href="#link2">
-                    Link 2
-                </ListGroup.Item>
-            </Accordion.Body>
-        </Accordion.Item>
-
-        {/*
-            props.elemente.map((element, index) => (
-                console.log(element)
-            ))
-
-                
+        {
+            Object.entries(liste).map(([key, values], index) => (
                 <Accordion.Item eventKey={index}>
-                    <Accordion.Header>{element}</Accordion.Header>
-                    <Accordion.Body>
-                      
-                    </Accordion.Body>
-                </Accordion.Item>
-                
-            ))
-               
-            liste.Entries
-            liste.fromEntries(
-                Object.entries(liste).map(([Key, value]) => (
-                console.log(key);
-            )
-        )
-             */
-
-        Object.entries(liste).forEach(([key, value]) => (  
-            
-            <Accordion.Item eventKey={key}>
                     <Accordion.Header>{key}</Accordion.Header>
                     <Accordion.Body>
-                       {/*console.log(`Key: ${key}`)
-                        console.log(`Values: ${value.join(', ')}`);
-                       */}
+                        
+                        {
+                        /* Elemente, die in key befinden. */
+                        values.map(value => (
+                            <ListGroup.Item action href="#link2">
+                                {value}
+                            </ListGroup.Item>
+                        ))
+                        
+                        }
+                            
                     </Accordion.Body>
-            </Accordion.Item>
-        ))
-
-    }
- 
+                </Accordion.Item>
+            ))
         
+        }
 
-        <Accordion.Item eventKey="2">
-           <Accordion.Header>Accordion Item #2</Accordion.Header>
-        </Accordion.Item>
     </Accordion>
   )
 }
--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -4,21 +4,19 @@ import Navigator from '../components/Navigator'
 
 export default function Mainlayout() {
     
-   const  element = ["Kinder", "Eltern", "Mitarbeiter", "Standort", "Gruppe"]
-
    const verwaltungsListe = {
     "Kinder": ["Übersicht", "Kind Bearbeiten", "Kind Löschen"],
-    "Eltern": ["Übersicht", "Eltern Bearbeiten", "Eltern Löschen"]
+    "Eltern": ["Übersicht", "Eltern Bearbeiten", "Eltern Löschen"],
+    "Mitarbeiter": ["Übersicht", "Mitarbeiter Bearbeiten", "Mitarbeiter Löschen"],
+    "Standort": ["Übersicht", "Standort Bearbeiten", "Standort Löschen"],
+    "Gruppe": ["Übersicht", "Gruppe Bearbeiten", "Gruppe Löschen"]
    }
    
   return (
    <Tab.Container id="list-group-tabs-example">
         <Row>
             <Col sm={4}>
-                <Navigator elemente={element}
-                
-                liste={verwaltungsListe}
-                />
+                <Navigator  liste={verwaltungsListe}/>
 
             </Col>
         </Row>
