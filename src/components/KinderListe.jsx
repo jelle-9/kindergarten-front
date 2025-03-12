aa1118791a71f8fda829b0b182f6985154bdeac4
ousmane sow <thifuge@gmail.com>
2025-03-12 12:09:47 +0100

    StandortListe fertig erstellt

--- src/components/KinderListe.jsx
+++ src/components/KinderListe.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Cards from '../layouts/Cards';
 import { Col, Row } from 'react-bootstrap';
 
 export default function KinderListe() {
--- src/components/StandortListe.jsx
+++ src/components/StandortListe.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Col, Row } from 'react-bootstrap';
 
 export default function StandortListe() {
   const [standorte, setStandort] = useState([]);
@@ -11,9 +14,32 @@ export default function StandortListe() {
   const loadStandorte = async() => {
     const result = await axios.get("http://localhost:8080/standort");
     console.log(result.data);
+    setStandort(result.data);
   }
 
   return (
-    <div>StandortListe</div>
+    <Row>
+        {
+            standorte.map((standort) => (
+
+                <Col  sm={4} md={3} lg={3} className='my-2'>
+                    <Card key = {standort.id} style={{ width: '10rem' }}>
+                        <Card.Img variant="top" src="resources\images.jpg" alt="Upps!" />
+                        <Card.Body>
+                            <Card.Title style={{fontSize: '15px', textAlign: 'center', fontWeight: 'bold'}}> {standort.standortName} </Card.Title>
+                            { <Card.Text style={{fontSize: '10px', whiteSpace: 'pre-line', textAlign: 'center'}}>
+                              
+                                {standort.standortTyp} {'\n'}
+                                {standort.standortStrasseHausnr}
+                            
+                            </Card.Text> }
+                            <Button style={{width: '7rem', height: '2rem', textAlign: 'center'}} >Bearbeiten</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))
+        }
+
+    </Row>
   )
 }
