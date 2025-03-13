fca6476af7e201c088c0b6b07962765bf820dc9a
EVGENY KVEST <evgenykvest@gmail.com>
2025-03-13 12:20:24 +0100

    Akkordeon WEG TSCHUSS

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,44 +1,12 @@
 import React from 'react'
-import Accordion from 'react-bootstrap/Accordion';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 
 export default function Navigator(props) {
 
     const liste = props.liste;
-   // const elemente = props.elemente;
+
   return (
-//    { <Accordion>
-//         <ListGroup>
-//             <ListGroup.Item action href="#link1">
-//                 Dashbord
-//             </ListGroup.Item>
-//         </ListGroup>
-
-//         {
-//             Object.entries(liste).map(([key, values], index) => (
-//                 <Accordion.Item eventKey={index}>
-//                     <Accordion.Header>{key}</Accordion.Header>
-//                     <Accordion.Body>
-                        
-//                         {
-//                         /* Elemente, die in key befinden. */
-//                         values.map(value => (
-//                             <ListGroup.Item action href="#link2">
-//                                 {value}
-//                             </ListGroup.Item>
-//                         ))
-                        
-//                         }
-                            
-//                     </Accordion.Body>
-//                 </Accordion.Item>
-//             ))
-        
-//         }
-
-//     </Accordion>}
-
     <ListGroup>
         <ListGroup.Item action href="#link1">
             Dashbord
@@ -46,7 +14,7 @@ export default function Navigator(props) {
 
         {
             Object.entries(liste).map(([key, values], index) => (
-                <ListGroup.Item key={index} action href={`#${key}`}>
+                <ListGroup.Item action href={`#${key}`}>
                    {key}
                 </ListGroup.Item>
             ))
