046b3258f10a467ca70cd947e1d7909d71773f4b
Jamal Helle <jamaluddin.helle@gmail.com>
2025-03-13 15:13:12 +0100

    axios POST für Kind erfolgreich
    Co-authored-by: thiewere <thiewere@users.noreply.github.com>
    Co-authored-by: Angelina Atmatsidou <aggelina.a@gmail.com>
    Co-authored-by: Evgeny Kvest <SkoofyDoo@users.noreply.github.com>

--- src/components/Formular.jsx
+++ src/components/Formular.jsx
@@ -2,34 +2,73 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import axios from 'axios';
+
+
+// const {data} = await axios.post('http://localhost:8080/addKind', document.querySelector('#my-form'), {
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+    
+//   })
 
 function Formular() {
+  console.log(document.querySelector('#my-form'))
+
+  const daten = (e) => {
+      const vorname = document.getElementById("kindVorname").value
+      const nachname = document.getElementById("kindNachname").value
+      const besonderheiten = document.getElementById("kindBesonderheiten").value
+      const geburtsdatum = document.getElementById("kindGeburtsdatum").value
+
+     const result =  axios.post('http://localhost:8080/addKind', {
+        kindVorname: vorname,
+        kindNachname: nachname,
+        besonderheiten: besonderheiten,
+        kindGeburtsdatum: geburtsdatum,
+        
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      console.log(result);
+      console.log(geburtsdatum)      
+      }
+  
+
   return (
-    <Form>
+    <Form id='my-form'>
       <fieldset>
         <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Group as={Col} >
             <Form.Label>Vorname des Kindes</Form.Label>
-            <Form.Control type="vorname" placeholder="Vorname *" />
+            <Form.Control id="kindVorname" type="vorname" placeholder="Vorname *" />
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Group as={Col}>
             <Form.Label>Nachname des Kindes</Form.Label>
-            <Form.Control type="nachname" placeholder="Nachname *" />
+            <Form.Control id="kindNachname" type="nachname" placeholder="Nachname *" />
           </Form.Group>
         </Row>
 
-        <Form.Group as={Col} controlId="formGridEmail">
+        <Form.Group as={Col}>
             <Form.Label>Besonderheiten</Form.Label>
-            <Form.Control rows={3} as="textarea" type="besonderheiten" placeholder="Besonderheiten" />
-          </Form.Group>
+            <Form.Control id="kindBesonderheiten" rows={3} as="textarea" type="besonderheiten" placeholder="Besonderheiten" />
+        </Form.Group>
 
+        <Form.Group as={Col}>
+            <Form.Label>Geburtsdatum</Form.Label>
+            <Form.Control id="kindGeburtsdatum" type="date" placeholder="Geburtsdatum" />
+        </Form.Group>
 
-
-        <Row>
+        {/* <Row>
           <Form.Group as={Col} className="mb-3" controlId="formGridAddress1">
             <Form.Label>Addresse</Form.Label>
-            <Form.Control placeholder="Straße Hausnummer" />
+            <Form.Control  placeholder="Straße Hausnummer" />
           </Form.Group>
 
           <Form.Group as={Col} className="mb-3" controlId="formGridAddress2">
@@ -57,9 +96,9 @@ function Formular() {
 
         <Form.Group className="mb-3" id="formGridCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        </Form.Group> */}
 
-        <Button variant="primary m-auto" type="submit">
+        <Button onClick={daten} variant="primary m-auto" type="submit">
           Hinzufügen
         </Button>
       </fieldset>
